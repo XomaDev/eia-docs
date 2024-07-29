4da017ccb79e0b2b3b83fd8593d581d54165e49d
kumaraswamy <care.xoma@gmail.com>
2024-07-29 22:01:10 +0530

    Begin writing eia docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Project Eia</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/XomaDev/eia-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Eia Docs',
   },
 }
 
